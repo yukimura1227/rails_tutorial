fa969e2136150e85696914b1c04e8bcde53088be
yukimura1227 <takamura1227@gmail.com>
2017-03-18 03:06:55 +0000

    practice 1.3.4-2

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "Hola, mundo!"
+    render html: "¡Hola, mundo!"
   end
 end
